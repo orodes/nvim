d4ed9bdad9f1d4c41a074128a7c06661ba8853b9
orodes <github@omnecloud.com>
2025-11-04 15:14:11 +1100

    feat: set backup/swap/undo folders to be stored locally to avoid issues if in an rclone mount

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,23 @@
 -- Add any additional options here
 vim.o.background = "light"
 vim.o.termguicolors = true
+
+-- ensure local dirs for swap/backup/undo exist and use them
+local data_home = vim.fn.stdpath("data")
+local dirs = { "swap", "backup", "undo" }
+
+for _, d in ipairs(dirs) do
+  local dir = data_home .. "/" .. d
+  if vim.fn.isdirectory(dir) == 0 then
+    vim.fn.mkdir(dir, "p")
+  end
+end
+
+vim.opt.directory:prepend(data_home .. "/swap//")
+vim.opt.backupdir:prepend(data_home .. "/backup//")
+vim.opt.undodir:prepend(data_home .. "/undo//")
+
+vim.opt.backup = true
+vim.opt.writebackup = true
+vim.opt.swapfile = true
+vim.opt.undofile = true
